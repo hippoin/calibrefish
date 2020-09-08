38dd5483055c49ce620a1b6df7e81be8b955e77a
Hipposin <65700227+Hipposin@users.noreply.github.com>
2020-09-08 23:01:00 +0530

    Initial Commit

---
+++ ProjectTest/app.js
@@ -0,0 +1,37 @@
+// const sr = ScrollReveal();
+window.sr = ScrollReveal({
+  reset: true,
+});
+sr.reveal("article", {
+  duration: 2000,
+  origin: "bottom",
+  distance: "20px",
+});
+
+sr.reveal(".bn-left", {
+  duration: 3000,
+  origin: "bottom",
+  distance: "20px",
+});
+
+// sr.reveal(".card", {
+//   duration: 4000,
+//   origin: "bottom",
+//   distance: "20px",
+// });
+
+sr.reveal(".rows", {
+  duration: 4000,
+  origin: "bottom",
+  distance: "20px",
+});
+// sr.reveal('.services',{
+
+//     duration:12000,
+//     origin: 'bottom',
+//     distance: '50px'
+
+// }
+// );
+
+// console.log("Hi This Is Raam");
